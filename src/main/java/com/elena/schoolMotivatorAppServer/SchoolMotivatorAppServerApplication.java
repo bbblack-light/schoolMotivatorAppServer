f7715f16c4da72de6a7dee7e7a2d678f11c91d76
BarakovaES <barakova.2000@mail.ru>
2021-07-12 19:14:13 +0400

    initial commit

---
+++ src/main/java/com/elena/schoolMotivatorAppServer/SchoolMotivatorAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.elena.schoolMotivatorAppServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMotivatorAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMotivatorAppServerApplication.class, args);
+	}
+
+}
